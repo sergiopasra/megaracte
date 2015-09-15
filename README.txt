50dbe7c4d6e1640d071d1a3f1182225d66ae16c6
Sergio Pascual <sergiopr@fis.ucm.es>
2015-09-15 13:40:03 +0200

    Initial files

---
+++ README.txt
@@ -0,0 +1,1 @@
+Reimplementation of the MEGARA ETC
---
+++ setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import find_packages, setup
+
+
+setup(name='megaracte',
+      version='0.1.dev0',
+      description='MEGARA CTE',
+      url='https://github.com/sergiopasra/megaracte',
+      license='GPLv3',
+      packages=find_packages(),
+      entry_points={'console_scripts': 
+                      ['megaracte = megaracte.cli:main']
+      },
+      classifiers=[
+        "Programming Language :: Python",
+        'Development Status :: 3 - Alpha',
+        "Environment :: Other Environment",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        ],
+     long_description=open('README.txt').read()
+)
